d83c1c9e6cb0f8c6c51d59c9e5d84b3cc6223080
nrwr434 <93894413+nrwr434@users.noreply.github.com>
2022-04-08 16:39:46 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -16,11 +16,16 @@ $(function() {
     });
     if(n == 0) {
       $('.graphic').hide();
+      $('.gr_btn').removeClass('on')
       $('.coding').show();
+      $('.co_btn').addClass('on')
     }
     else {
       $('.coding').hide();       
+      $('.co_btn').removeClass('on')
       $('.graphic').show();
+      $('.gr_btn').addClass('on')
+      $('.graphic ul').masonry()
     }
   })
 
@@ -44,6 +49,66 @@ $(function() {
       }
    })
 
+  //  타이핑
+  // var typing = document.getElementById('p_ty');
+ 
+  // var typewriter = new Typewriter(typing, {
+  //     loop: false, delay: 1000
+  // });
+   
+  // typewriter.typeString('안녕하세요,')      
+  //     // .pauseFor(2500)
+  //     // .deleteAll()
+  //     // .typeString('신입 웹디자이너 & 웹퍼블리셔')
+  //     // .pauseFor(2500)
+  //     // .deleteAll()
+  //     // .typeString('최우찬 입니다.')
+  //     .start();
+  
+  var typingBool = false; 
+  var typingIdx=0; 
+  var liIndex = 0;
+  var liLength = $(".typing-txt>ul>li").length;
+
+  // 타이핑될 텍스트를 가져온다 
+  var typingTxt = $(".typing-txt>ul>li").eq(liIndex).text(); 
+  typingTxt=typingTxt.split(""); // 한글자씩 자른다. 
+  if(typingBool==false){ // 타이핑이 진행되지 않았다면 
+      typingBool=true; 
+      var tyInt = setInterval(typing,100); // 반복동작 
+  } 
+      
+function typing(){ 
+  $(".typing ul li").removeClass("on");
+   $(".typing ul li").eq(liIndex).addClass("on");
+  if(typingIdx<typingTxt.length){ // 타이핑될 텍스트 길이만큼 반복 
+     $(".typing ul li").eq(liIndex).append(typingTxt[typingIdx]); // 한글자씩 이어준다. 
+     typingIdx++; 
+   } else{ if(liIndex<liLength-1){
+     //다음문장으로  가기위해 인덱스를 1증가
+       liIndex++; 
+     //다음문장을 타이핑하기위한 셋팅
+        typingIdx=0;
+        typingBool = false; 
+        typingTxt = $(".typing-txt>ul>li").eq(liIndex).text(); 
+       
+     //다음문장 타이핑전 1초 쉰다
+         clearInterval(tyInt);
+          //타이핑종료
+     
+         setTimeout(function(){
+           //1초후에 다시 타이핑 반복 시작
+           tyInt = setInterval(typing,100);
+         },1000);
+        } else if(liIndex==liLength-1){
+          
+         //마지막 문장까지 써지면 반복종료
+           clearInterval(tyInt);
+        }
+    } 
+}  
+  // 마지막문장이 느림
+  
   //Project 탭
   $('.co_btn').click(function() {
       $('.graphic').hide();
